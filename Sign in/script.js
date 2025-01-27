2a8449a9d12878c0a44f193a74b28c7c875b3cf4
jerie <lacapjerie@gmail.com>
2025-01-27 16:14:26 +0800

    initial commit

---
+++ Sign in/script.js
@@ -0,0 +1,8 @@
+const togglePassword = document.querySelector('.toggle-password');
+const passwordInput = document.querySelector('#password');
+
+togglePassword.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    togglePassword.textContent = type === 'password' ? '👁️' : '🙈';
+});
---
+++ Sign up/script.js
@@ -0,0 +1,8 @@
+const togglePassword = document.querySelector('.toggle-password');
+const passwordInput = document.querySelector('#confirm-password');
+
+togglePassword.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    togglePassword.textContent = type === 'password' ? '👁️' : '🙈';
+});
