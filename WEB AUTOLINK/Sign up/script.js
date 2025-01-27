a04af5e21ec610d587e333d5f95451478f82ae17
Rejieee <148456573+Rejieee@users.noreply.github.com>
2025-01-27 09:18:46 +0800

    Delete WEB AUTOLINK directory

--- WEB AUTOLINK/Sign up/script.js
+++
@@ -1,8 +0,0 @@
-const togglePassword = document.querySelector('.toggle-password');
-const passwordInput = document.querySelector('#confirm-password');
-
-togglePassword.addEventListener('click', () => {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    togglePassword.textContent = type === 'password' ? '👁️' : '🙈';
-});
